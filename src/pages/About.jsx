f328ebf179c5bed01f60c7bc0f651861b20a777f
Stella <stellamdoda02@gmail.com>
2024-06-09 18:05:36 +0200

    npm

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+// AboutUs.jsx
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our recipe website! We are passionate about cooking and sharing delicious recipes with you.</p>
+      <p>Feel free to explore our collection of mouthwatering dishes from around the world.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
